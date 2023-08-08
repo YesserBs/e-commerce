49d2a88b802a4ef1a37839ba26978f93ab20c0b8
Yesser <benselmayasser1@gmail.com>
2023-08-08 12:21:45 +0200

    +Custom toggleButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,95 +1,36 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:myproj/config/configuration.dart';
+import 'package:myproj/main_settings/appPages.dart';
+import 'package:myproj/view/auth/auth.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await GetStorage.init();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            MovingWidget(),
-          ],
-        ),
+    ScreenUtil.init(context, designSize: Size(1080, 2340));
+    return GetMaterialApp(
+      initialRoute: auth(),
+      theme: ThemeData(colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: config.secondaryColor),
       ),
+      getPages: AppPages.list,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MovingWidget extends StatefulWidget {
-  @override
-  _MovingWidgetState createState() => _MovingWidgetState();
-}
 
-class _MovingWidgetState extends State<MovingWidget> {
-  bool _isLeft = false;
 
-  void _togglePosition() {
-    setState(() {
-      _isLeft = !_isLeft;
-    });
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          height: 37,
-          width: 202,
-          decoration: BoxDecoration(
-              color: Colors.grey[500],
-              borderRadius: BorderRadius.circular(5)
-          ),
-        ),
-
-        AnimatedPositioned(
-          top: 1,
-          duration: Duration(milliseconds: 450),
-          curve: Curves.easeInOut,
-          left: _isLeft ? 2.0 : 100.0,
-          child: GestureDetector(
-            onTap: _togglePosition,
-            child: Container(
-              width: 100,
-              height: 35,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(5)
-              ),
-            ),
-          ),
-        ),
-        Positioned(
-            top: 9,
-            left: 38,
-            child: GestureDetector(
-              onTap: () => _togglePosition(),
-              child: Text("Left", style: TextStyle(
-                  color: !_isLeft
-                      ?Colors.grey[400]: Colors.grey[500] )
-              ),
-            )
-        ),
-        Positioned(
-            top: 9,
-            right: 38,
-            child: GestureDetector(
-              onTap: () => _togglePosition(),
-              child: Text("Right", style: TextStyle(
-                  color: _isLeft
-                      ?Colors.grey[400]: Colors.grey[500])
-              ),
-            )
-        ),
-
-      ],
-    );
-  }
-}

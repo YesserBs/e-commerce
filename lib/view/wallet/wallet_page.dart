5af41deb0ddc0ee53650d03928da364353fc0b3d
Yesser <benselmayasser1@gmail.com>
2023-08-08 15:09:24 +0200

    +List of static transactions

--- lib/view/wallet/wallet_page.dart
+++ lib/view/wallet/wallet_page.dart
@@ -6,6 +6,8 @@ import 'package:myproj/config/configuration.dart';
 import 'package:myproj/custom/cToggleButton.dart';
 
 class WalletPage extends StatelessWidget {
+  final yourList = ["Purchased articles: 4", "Purchased articles: 2", "Purchased articles: 4", "Purchased articles: 1"];
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -50,39 +52,39 @@ class WalletPage extends StatelessWidget {
                 Positioned(
                   bottom: -20,
                   child: Container(
-                    
+
                     height: 60,
                     width: 411,
                     decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(25)
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(25)
                     ),
                   ),
                 ),
                 /* I have to make a fonction that returns a Button wwith
                 the icon because there is a lot of repetition*/
                 Positioned(
-                  left: 50,
+                    left: 50,
                     bottom: 10,
                     child: Material(
-                  elevation: 1.8, // Adjust the elevation value as needed
-                  shape: CircleBorder(),
-                  color: Colors.white,
-                  child: SizedBox(
-                    width: 63,
-                    height: 65,
-                    child: IconButton(
-                      icon: Icon(
-                        CupertinoIcons.plus,
-                        color: Colors.grey[700],
-                        size: 25,
+                      elevation: 1.8, // Adjust the elevation value as needed
+                      shape: CircleBorder(),
+                      color: Colors.white,
+                      child: SizedBox(
+                        width: 63,
+                        height: 65,
+                        child: IconButton(
+                          icon: Icon(
+                            CupertinoIcons.plus,
+                            color: Colors.grey[700],
+                            size: 25,
+                          ),
+                          onPressed: () {
+                            // Your onPressed action here
+                          },
+                        ),
                       ),
-                      onPressed: () {
-                        // Your onPressed action here
-                      },
-                    ),
-                  ),
-                )
+                    )
 
                 ),
                 Positioned(
@@ -152,10 +154,46 @@ class WalletPage extends StatelessWidget {
               height: 410,
               width: 340,
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: config.lightPrimaryColor,
+                borderRadius: BorderRadius.circular(15),
+                //color: config.lightRed,
+              ),
+              child: ListView.builder(
+                itemCount: yourList.length, // Replace with the actual number of elements in your list
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    margin: EdgeInsets.symmetric(vertical: 3, horizontal: 3),
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,boxShadow: [
+                      BoxShadow(
+                        color: config.lightPrimaryColor, // Shadow color
+                        spreadRadius: 1, // How much the shadow spreads
+                        blurRadius: 2,   // The blur effect of the shadow
+                        offset: Offset(0.5, 0.5), // Offset in x and y direction
+                      ),
+                    ],
+
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          yourList[index], // Replace with your actual data
+                          style: TextStyle(fontSize: 15, ),
+                        ),
+                        Text(
+                          "08/08/2023", // Replace with your actual data
+                          style: TextStyle(fontSize: 15, color: config.primaryColor),
+                        ),
+                      ],
+                    ),
+                  );
+                },
               ),
             )
+
+
 
           ],
         ),
@@ -163,5 +201,4 @@ class WalletPage extends StatelessWidget {
     );
   }
 }
-
 

f5c0e5dbf7d8cd578b100471ba0045e2fd12faa5
Yesser <benselmayasser1@gmail.com>
2023-08-10 20:23:26 +0200

    Voucers added to VoucerPage but still can't be expanded

--- lib/view/voucer/voucer_page.dart
+++ lib/view/voucer/voucer_page.dart
@@ -13,6 +13,7 @@ class VoucerPage extends StatelessWidget {
   //VoucerController VC = Get.find();
   ProfileController PC = Get.find();
   late ToggleButton TButton;
+  var Added = ["assets/images/coupon.png", "assets/images/coupon2.png", "assets/images/coupon1.png", "assets/images/coupon.png", "assets/images/coupon2.png", "assets/images/coupon1.png", ];
 
   VoucerPage() {
     TButton = ToggleButton(
@@ -31,29 +32,92 @@ class VoucerPage extends StatelessWidget {
         dismissOnCapturedTaps: true,
         child: Scaffold(
           backgroundColor: Colors.white,
-          body: Padding(
-            padding: EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text("Add a coupon with code"),
-                60.h.verticalSpace,
-                SearchFormField('Voucer applied successfully'),
-                60.h.verticalSpace,
-                Text("Scan with QR code"),
-                40.h.verticalSpace,
-                Icon(
-                  Icons.qr_code_scanner_outlined,
-                  size: 70,
-                  color: config.primaryColor,
+          body: Column(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text("Add a coupon with code"),
+                      60.h.verticalSpace,
+                      SearchFormField('Voucer applied successfully'),
+                      60.h.verticalSpace,
+                      Text("Scan with QR code"),
+                      40.h.verticalSpace,
+                      Container(
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: config.primaryColor.withOpacity(0.3), // Shadow color
+                              spreadRadius: 1, // How far the shadow extends
+                              blurRadius: 10, // The blur radius
+                              offset: Offset(0.5, 0.5), // Offset of the shadow
+                            ),
+                          ],
+                          //boxShadow:
+                        ),
+                        child: Icon(
+                          Icons.qr_code_scanner_outlined,
+                          size: 70,
+                          color: config.primaryColor,
+                        ),
+                      ),
+                      240.h.verticalSpace,
+                      Text("Your voucers"),
+                      60.h.verticalSpace,
+                      TButton,
+                      120.h.verticalSpace,
+                      Expanded(
+                        child: Obx(() => ListView.builder(
+                          itemCount:
+                          PC.isLeft.value
+                              ? Added.length
+                              : Added.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return
+                              Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 3, horizontal: 15),
+                                child: Row(
+                                  children: [
+                                    Expanded(
+                                      child: Container(
+                                        margin: EdgeInsets.symmetric(horizontal: 4),
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(10),
+                                          color: Colors.red,
+                                          boxShadow: [
+                                          BoxShadow(
+                                            color: config.lightPrimaryColor, // Shadow color
+                                            spreadRadius: 1, // How much the shadow spreads
+                                            blurRadius: 2,   // The blur effect of the shadow
+                                            offset: Offset(0.5, 0.5), // Offset in x and y direction
+                                          ),
+                                        ],
+                                        ),
+                                        child: Image.asset(Added[index]),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              );
+                          },
+                        ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-                240.h.verticalSpace,
-                Text("Your voucers"),
-                60.h.verticalSpace,
-                TButton,
-
-              ],
-            ),
+              ),
+              Container(
+                height: 30,
+                color: Colors.white,
+              )
+            ],
           ),
         ),
       ),
